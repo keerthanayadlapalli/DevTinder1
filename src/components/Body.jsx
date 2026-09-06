78ea3629bb6510f3e7c5a5fdd97363f3adeab761
keerthana <keerthanayadlapalli896@gmail.com>
2026-09-06 23:49:54 +0530

    Implemented authentication persistence by checking the user's authentication status when the application loads. If a valid JWT token is present, the user's data is fetched and stored in Redux, allowing them to remain logged in. If authentication fails or no valid token exists, the user is redirected to the Login page.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,31 @@
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
+import axios from 'axios';
+import Base_URL from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () =>{
+    try{
+        const res = await axios.get(Base_URL + "/profile/view",{withCredentials : true});
+        dispatch(addUser(res.data));
+    }
+    catch(err){
+      if(err.response && err.response.status === 401){
+      navigate('/login');
+      }
+      console.log(err);
+    }
+  }
+  useEffect(()=>{
+    fetchUser();
+  },[]); 
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useSelector} from 'react-redux';
 const Navbar = () => {
   const user = useSelector(store => store.user);
-  console.log(user);
+  console.log(user); 
   return (
       <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
