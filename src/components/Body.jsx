0016c28eea83458fb5cffbaba12c804a12c252d9
keerthana <keerthanayadlapalli896@gmail.com>
2026-09-07 22:51:32 +0530

    If it is a logged in user, why to call fetchUser multiple times so we will check the store

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,25 +7,30 @@ import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
   const fetchUser = async () =>{
     try{
         const res = await axios.get(Base_URL + "/profile/view",{withCredentials : true});
-        dispatch(addUser(res.data));
+        dispatch(addUser(res.data)); 
     }
     catch(err){
-      if(err.response && err.response.status === 401){
-      navigate('/login');
+      if(err.status === 401){
+        navigate('/login');
       }
       console.log(err);
     }
   }
   useEffect(()=>{
+    if(!userData){
     fetchUser();
-  },[]); 
+    }
+  },[]);
+  
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
