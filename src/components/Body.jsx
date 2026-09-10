83d909f2e6fd586db7c10e309e04794e87ba5b68
keerthana <keerthanayadlapalli896@gmail.com>
2026-09-10 23:36:57 +0530

    Why to make multiple API calls, when the user already exists in the redux store. so, we have fixed this and also if the user, who is not logged-in. if he tries to login the user should redirect to login page this is also fixed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,29 +7,31 @@ import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const userData = useSelector((store) => store.user);
-  const fetchUser = async () =>{
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchUser = async () => {
     try{
         const res = await axios.get(Base_URL + "/profile/view",{withCredentials : true});
         dispatch(addUser(res.data)); 
     }
     catch(err){
-      if(err.status === 401){
+      if(err.response?.status === 401){
         navigate('/login');
       }
       console.log(err);
+    } finally {
+      setIsLoading(false);
     }
   }
   useEffect(()=>{
-    if(!userData){
-    fetchUser(); 
-    }
+   fetchUser(); 
   },[]);
+
+  if (isLoading) return null;
   
   return (
     <div>
