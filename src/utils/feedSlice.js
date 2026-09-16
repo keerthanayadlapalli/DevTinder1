f6e36d2e0147a9cc178b2c80d29394d70fa79a0e
keerthana <keerthanayadlapalli896@gmail.com>
2026-09-16 22:48:07 +0530

    RemoveFeed action added

--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -5,8 +5,13 @@ const feedSlice = createSlice({
     reducers:{
         addFeed: (state,action) =>{
             return action.payload;
+        },
+        removeFeed: (state,action) =>{
+            return null;
         }
+        
+        
     }
 })
 export default feedSlice.reducer;
-export const {addFeed} = feedSlice.actions;
+export const {addFeed, removeFeed} = feedSlice.actions;
