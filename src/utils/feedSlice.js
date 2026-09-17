f3094818ea6feed0c395b3c9681198ac746572b5
keerthana <keerthanayadlapalli896@gmail.com>
2026-09-17 23:34:24 +0530

    FeedSlice reducers arrow functions

--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -3,14 +3,8 @@ const feedSlice = createSlice({
     name: "feed",
     initialState: null,
     reducers:{
-        addFeed: (state,action) =>{
-            return action.payload;
-        },
-        removeFeed: (state,action) =>{
-            return null;
-        }
-        
-        
+        addFeed: (state,action) => action.payload,
+        removeFeed: (state,action) => null
     }
 })
 export default feedSlice.reducer;
